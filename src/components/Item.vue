7193ab2015d2d8ff7869d454d2f12f6a1f1037cf
Marcelo Eleuterio <marceloeleuterio@outlook.com.br>
2021-01-10 20:40:11 -0300

    subindo app de tarefas

---
+++ src/components/Item.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="item">
+        <ul>
+            <li v-for="task in lista" :key="task.key">
+                <span>{{ task.text }}</span>
+                <button @click="concluirTask(task.key)">Concluir</button>
+            </li>
+        </ul>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Item',
+    props: [
+        'lista',
+        'delete'
+    ],
+    methods: {
+        concluirTask(key) {
+            this.delete(key)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #item {
+        white-space: pre-wrap;
+        word-wrap: break-word;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin-top: 20px;
+    }
+
+    ul li {
+        padding: 15px 0;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    li:not(:first-child) {
+        border-top: 1px solid #E7DECD;
+    }
+
+    span {
+        color: #FBFAF8;
+    }
+
+    button {
+        cursor: pointer;
+        background: #E7DECD;
+        color: #0A122A;
+        border: 0;
+        padding: 8px 8px;
+        outline: none;
+        border-radius: 4px;
+    }
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,107 @@
+<template>
+    <div id="task">
+        <form @submit.prevent="addTarefa">
+            <input type="text" placeholder="Tarefa de hoje?" v-model="tarefa"/>
+            <button type="submit"><strong>Adicionar</strong></button>
+        </form>
+
+        <item :lista="tarefas" :delete="apagarTask"/>
+
+        <span v-show="tarefas.length > 0">
+            Você tem <strong :class="{pend: pendente}"> {{ tarefas.length }} </strong> tarefas pendentes.
+        </span>
+    </div>
+</template>
+
+<script>
+import Item from './Item'
+
+export default {
+    name: 'Task',
+    components: {
+        Item
+    },
+    data() {
+        return {
+            tarefa: '',
+            tarefas: [],
+            pendente: false
+        }
+    },
+    methods: {
+        addTarefa() {
+            if(this.tarefa !== '') {
+                this.tarefas.push({
+                    text: this.tarefa,
+                    key: Date.now()
+                })
+            } else {
+                alert('Digite uma tarefa...')
+                return
+            }
+            this.tarefa = ''
+            console.log(this.tarefas)
+        },
+        apagarTask(key) {
+            let filtro = this.tarefas.filter( (item) => { //salvar dentro de filtro, todas tarefas diferentes da key passada
+                return (item.key !== key)
+            })
+
+            return this.tarefas = filtro
+        }
+    },
+    watch: {
+        tarefas() {
+            localStorage.setItem('tasks', JSON.stringify(this.tarefas))
+            this.tarefas.length > 4 ? this.pendente = true : this.pendente = false
+        },
+    },
+    created() { //chamado automaticamente sempre que a instancia do Vue for criada
+        const json = localStorage.getItem('tasks')
+        this.tarefas = JSON.parse(json) || []
+    }
+}
+</script>
+
+<style scoped>
+    #task {
+        max-width: 700px;
+        background: #698F3F;
+        border-radius: 4px;
+        padding: 20px;
+        margin: 20px auto;
+        box-shadow: 0 0 20px rgba(0,0,0);
+    }
+
+    form {
+        margin-top: 30px;
+        display: flex;
+        flex-direction: row;
+    }
+
+    form button {
+        cursor: pointer;
+        background: #E7DECD;
+        border: 0;
+        border-radius: 4px;
+        margin-left: 10px;
+        padding: 0 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #0A122A;
+    }
+
+    input {
+        flex: 1;
+        border: 1px solid #eee;
+        font-size: 14px;
+        border-radius: 4px;
+        outline: none;
+        padding: 6px 10px;
+    }
+
+    .pend {
+        color: #9e0e0e;
+    }
+</style>
